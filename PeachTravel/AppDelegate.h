3df7f3042fcd2f6069408e94ab1381fb60379430
Hacker-liang <pengshuai.liang@gmai.com>
2014-09-16 20:09:49 +0800

    Initial Commit

---
+++ PeachTravel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PeachTravel
+//
+//  Created by liangpengshuai on 9/16/14.
+//  Copyright (c) 2014 com.aizou.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PeachTravel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PeachTravel
+//
+//  Created by liangpengshuai on 9/16/14.
+//  Copyright (c) 2014 com.aizou.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
