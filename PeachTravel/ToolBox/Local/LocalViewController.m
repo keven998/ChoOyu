08165ea950e48ad8ec6cf638f72463e162f356af
罗勇 <luoyong@lvxingpai.cn>
2014-12-02 21:02:22 +0800

    周边初始化备份

--- PeachTravel/ToolBox/Local/LocalViewController.m
+++ PeachTravel/ToolBox/Local/LocalViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "LocalViewController.h"
+#import "DMFilterView.h"
+#import "SwipeView.h"
 
-@interface LocalViewController ()
+#define LOCAL_PAGE       @[@"当地美食", @"shopping", @"游玩", @"酒店"]
+
+@interface LocalViewController ()<DMFilterViewDelegate, SwipeViewDataSource, SwipeViewDelegate, UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) DMFilterView *filterView;
+@property (nonatomic, strong) SwipeView *swipeView;
 
 @end
 
@@ -19,6 +26,100 @@
     // Do any additional setup after loading the view.
     self.navigationItem.title = @"我身边";
     self.view.backgroundColor = APP_PAGE_COLOR;
+    
+    _filterView = [[DMFilterView alloc]initWithStrings:LOCAL_PAGE containerView:self.view];
+    _filterView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [self.filterView attachToContainerView];
+    [self.filterView setDelegate:self];
+    
+    _swipeView = [[SwipeView alloc] initWithFrame:CGRectMake(0, 64.0 + CGRectGetHeight(_filterView.frame), CGRectGetWidth(self.view.bounds), CGRectGetHeight(self.view.bounds) - 64.0 - CGRectGetHeight(_filterView.frame))];
+    _swipeView.dataSource = self;
+    _swipeView.delegate = self;
+    _swipeView.bounces = NO;
+    _swipeView.backgroundColor = [UIColor redColor];
+    _swipeView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+    _swipeView.pagingEnabled = YES;
+    _swipeView.itemsPerPage = 1;
+    [self.view addSubview:_swipeView];
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 0.0;
+}
+
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return nil;
+}
+
+#pragma mark - SwipeViewDataSource
+- (NSInteger)numberOfItemsInSwipeView:(SwipeView *)swipeView {
+    return LOCAL_PAGE.count;
+}
+
+- (UIView *)swipeView:(SwipeView *)swipeView viewForItemAtIndex:(NSInteger)index reusingView:(UIView *)view {
+    UITableView *tbView = nil;
+    
+    //create new view if no view is available for recycling
+    if (view == nil)
+    {
+        view = [[UIView alloc] initWithFrame:self.swipeView.bounds];
+        view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        
+        tbView = [[UITableView alloc] initWithFrame:view.bounds];
+        tbView.separatorStyle = UITableViewCellSeparatorStyleNone;
+        tbView.dataSource = self;
+        tbView.delegate = self;
+        tbView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        tbView.tag = 1;
+        tbView.backgroundColor = APP_PAGE_COLOR;
+        [view addSubview:tbView];
+    }
+    else
+    {
+        tbView = (UITableView *)[view viewWithTag:1];
+    }
+    
+    //set background color
+    CGFloat red = arc4random() / (CGFloat)INT_MAX;
+    CGFloat green = arc4random() / (CGFloat)INT_MAX;
+    CGFloat blue = arc4random() / (CGFloat)INT_MAX;
+    tbView.backgroundColor = [UIColor colorWithRed:red
+                                           green:green
+                                            blue:blue
+                                           alpha:1.0];
+
+    
+    return view;
+}
+
+
+#pragma mark - SwipeViewDelegate
+
+- (void)swipeViewCurrentItemIndexDidChange:(SwipeView *)swipeView {
+    
+}
+
+- (CGSize)swipeViewItemSize:(SwipeView *)swipeView {
+    return swipeView.bounds.size;
+}
+
+#pragma mark - DMFilterViewDelegate
+
+- (void)filterView:(DMFilterView *)filterView didSelectedAtIndex:(NSInteger)index {
+    
 }
 
 - (void)didReceiveMemoryWarning {
