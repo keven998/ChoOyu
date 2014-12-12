5b59673f0a382a53bc6840107f611198c06ef6b2
罗勇 <luoyong@lvxingpai.cn>
2014-12-12 18:29:50 +0800

    图集bug

--- Pods/MWPhotoBrowser/MWPhotoBrowser/Classes/MWPhotoBrowser.m
+++ Pods/MWPhotoBrowser/MWPhotoBrowser/Classes/MWPhotoBrowser.m
@@ -1092,12 +1092,12 @@
 
 - (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
 	// Hide controls when dragging begins
-	[self setControlsHidden:YES animated:YES permanent:NO];
+//	[self setControlsHidden:YES animated:YES permanent:NO];
 }
 
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
 	// Update nav when page changes
-	[self updateNavigation];
+//	[self updateNavigation];
 }
 
 #pragma mark - Navigation
