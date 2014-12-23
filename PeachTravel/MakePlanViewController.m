d982d24d3d79faae05085ce6eef7bbe86f0d6f0d
罗勇 <luoyong@lvxingpai.cn>
2014-12-23 16:04:26 +0800

    不保存中间页

--- PeachTravel/MakePlanViewController.m
+++ PeachTravel/MakePlanViewController.m
@@ -121,7 +121,10 @@
         poi1.zhName = @"上海大不列颠";
         tripDetailCtl.destinations = @[poi, poi1];
         tripDetailCtl.isMakeNewTrip = YES;
-        [self.navigationController pushViewController:tripDetailCtl animated:YES];
+//        [self.navigationController pushViewController:tripDetailCtl animated:YES];
+        NSMutableArray *array = [NSMutableArray arrayWithArray:[self.navigationController viewControllers]];
+        [array replaceObjectAtIndex:(array.count - 1) withObject:tripDetailCtl];
+        [self.navigationController setViewControllers:array animated:YES];
     } else {
         [SVProgressHUD showErrorWithStatus:@"请先登录"];
         [self performSelector:@selector(login) withObject:nil afterDelay:0.3];
