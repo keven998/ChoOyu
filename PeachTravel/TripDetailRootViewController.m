a5a247fe34efe81c46ba8d97889842bdb4ace8a4
罗勇 <luoyong@lvxingpai.cn>
2015-01-08 17:26:35 +0800

    文案

--- PeachTravel/TripDetailRootViewController.m
+++ PeachTravel/TripDetailRootViewController.m
@@ -82,7 +82,7 @@
     
     [self setupViewControllers];
     if (_isMakeNewTrip) {
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil message:@"是否需要小桃为你推荐行程，制作memo更简单" delegate:self cancelButtonTitle:@"不需要" otherButtonTitles:@"为我推荐", nil];
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil message:@"是否需要小桃为你推荐行程，制作memo更简单" delegate:self cancelButtonTitle:@"不需要" otherButtonTitles:@"给我推荐", nil];
         [alert showAlertViewWithBlock:^(NSInteger buttonIndex) {
             if (buttonIndex == 0) {
                 [self loadNewTripDataWithRecommendData:NO];
