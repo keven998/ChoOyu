589d0c633ee290baa50d2838197e536293f9d917
罗勇 <luoyong@lvxingpai.cn>
2015-01-27 21:22:42 +0800

    bug

--- PeachTravel/SpotDetailViewController.m
+++ PeachTravel/SpotDetailViewController.m
@@ -44,7 +44,7 @@
 
 - (void)updateView
 {
-    _spotDetailView = [[SpotDetailView alloc] initWithFrame:CGRectMake(5, 50, self.view.bounds.size.width-10, self.view.bounds.size.height-80)];
+    _spotDetailView = [[SpotDetailView alloc] initWithFrame:CGRectMake(5, 47, self.view.bounds.size.width-10, self.view.bounds.size.height-80)];
     _spotDetailView.spot = self.spotPoi;
     self.navigationItem.title = self.spotPoi.zhName;
     _spotDetailView.layer.cornerRadius = 4.0;
