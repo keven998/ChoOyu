3698f72382ccbbf784fd7c537c663dffbbe2913f
Hacker-liang <pengshuai.liang@gmai.com>
2015-02-09 18:47:12 +0800

    景点详情

--- PeachTravel/TravelPlan/PoiDetail/SpotDetail/SpotDetailViewController.m
+++ PeachTravel/TravelPlan/PoiDetail/SpotDetail/SpotDetailViewController.m
@@ -44,7 +44,7 @@
 
 - (void)updateView
 {
-    _spotDetailView = [[SpotDetailView alloc] initWithFrame:CGRectMake(5, 46, self.view.bounds.size.width-10, self.view.bounds.size.height-80)];
+    _spotDetailView = [[SpotDetailView alloc] initWithFrame:CGRectMake(15, 40, self.view.bounds.size.width-30, self.view.bounds.size.height-60)];
     _spotDetailView.spot = self.spotPoi;
     self.navigationItem.title = self.spotPoi.zhName;
     _spotDetailView.layer.cornerRadius = 4.0;
