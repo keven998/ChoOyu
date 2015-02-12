b14e66789b1c59acb4bc278cf08b3ef1c897cd4f
Hacker-liang <pengshuai.liang@gmai.com>
2015-02-12 18:55:11 +0800

    我身边 跳转地图 bug

--- PeachTravel/ToolBox/Local/LocalViewController.m
+++ PeachTravel/ToolBox/Local/LocalViewController.m
@@ -404,7 +404,7 @@
     PoisOfCityTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"poisOfCity"];
     cell.shouldEdit = NO;
     cell.isNearByCell = YES;
-    cell.tag = indexPath.row;
+    cell.naviBtn.tag = indexPath.row;
     [cell.naviBtn removeTarget:self action:@selector(jumpToMapView:) forControlEvents:UIControlEventTouchUpInside];
     [cell.naviBtn addTarget:self action:@selector(jumpToMapView:) forControlEvents:UIControlEventTouchUpInside];
     cell.poi = [[_dataSource objectAtIndex:page] objectAtIndex:indexPath.row];
