67fc6f9b67f5631468649de1c82c7f322066b261
Hacker-liang <pengshuai.liang@gmai.com>
2015-02-12 11:49:50 +0800

    搜索景点添加布局

--- PeachTravel/ToolBox/Search/SearchDestinationViewController.m
+++ PeachTravel/ToolBox/Search/SearchDestinationViewController.m
@@ -419,7 +419,7 @@ static NSString *reusableCellIdentifier = @"searchResultCell";
     PoiSummary *poi = [[[self.dataSource objectAtIndex:indexPath.section] objectForKey:@"content"] objectAtIndex:indexPath.row];
     SearchResultTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:reusableCellIdentifier];
 
-    if (poi.poiType == kRestaurantPoi || poi.poiType == kShoppingPoi || poi.poiType == kHotelPoi) {
+    if (poi.poiType == kRestaurantPoi || poi.poiType == kShoppingPoi || poi.poiType == kHotelPoi || poi.poiType == kSpotPoi) {
         TaoziImage *image = [poi.images firstObject];
         [cell.headerImageView sd_setImageWithURL:[NSURL URLWithString:image.imageUrl] placeholderImage:nil];
         cell.titleLabel.text = poi.zhName;
