9c74f69513842a629c75ef18af909a32a1bb646d
Hacker-liang <pengshuai.liang@gmai.com>
2015-03-30 15:14:21 +0800

    修改版本号

---
+++ PeachTravel/Common/Common/TZSideViewController.h
@@ -0,0 +1,13 @@
+//
+//  TZSideViewController.h
+//  PeachTravel
+//
+//  Created by liangpengshuai on 3/30/15.
+//  Copyright (c) 2015 com.aizou.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TZSideViewController : UIViewController
+
+@end
---
+++ PeachTravel/Common/Common/TZSideViewController.m
@@ -0,0 +1,37 @@
+//
+//  TZSideViewController.m
+//  PeachTravel
+//
+//  Created by liangpengshuai on 3/30/15.
+//  Copyright (c) 2015 com.aizou.www. All rights reserved.
+//
+
+#import "TZSideViewController.h"
+
+@interface TZSideViewController ()
+
+@end
+
+@implementation TZSideViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
