34cf511c250d53ce3b1aac30426f8e196343f92a
罗勇 <luoyong@lvxingpai.cn>
2015-05-19 20:15:43 +0800

    发录音

--- PeachTravel/IM/Chat/ChatView/MessageToolbar/DXMessageToolBar.m
+++ PeachTravel/IM/Chat/ChatView/MessageToolbar/DXMessageToolBar.m
@@ -107,6 +107,8 @@
 {
     if (_toolbarView == nil) {
         _toolbarView = [[UIView alloc] init];
+        _toolbarView.backgroundColor = APP_PAGE_COLOR;
+        _toolbarView.userInteractionEnabled = YES;
     }
     
     return _toolbarView;
@@ -354,23 +356,21 @@
     self.inputTextView.center = CGPointMake(CGRectGetWidth(self.toolbarView.frame)/2.0, CGRectGetHeight(self.toolbarView.frame)/2.0);
 
     //录制
-    self.recordButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, width*3/4, 34)];
+    self.recordButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, width-44, 34)];
     self.recordButton.titleLabel.font = [UIFont systemFontOfSize:15.0];
     [self.recordButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
-    [self.recordButton setBackgroundImage:[[UIImage imageNamed:@"chatbar_text_background@"] stretchableImageWithLeftCapWidth:10 topCapHeight:10] forState:UIControlStateNormal];
-    self.recordButton.layer.cornerRadius = 4.0;
+    [self.recordButton setBackgroundImage:[[UIImage imageNamed:@"chatbar_text_background"] stretchableImageWithLeftCapWidth:10 topCapHeight:10] forState:UIControlStateNormal];
     [self.recordButton setBackgroundImage:[[UIImage imageNamed:@"chatBar_recordSelectedBg"] stretchableImageWithLeftCapWidth:10 topCapHeight:10] forState:UIControlStateHighlighted];
     [self.recordButton setTitle:kTouchToRecord forState:UIControlStateNormal];
     [self.recordButton setTitle:kTouchToFinish forState:UIControlStateHighlighted];
     self.recordButton.hidden = YES;
-    self.recordButton.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    self.recordButton.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
     [self.recordButton addTarget:self action:@selector(recordButtonTouchDown) forControlEvents:UIControlEventTouchDown];
     [self.recordButton addTarget:self action:@selector(recordButtonTouchUpOutside) forControlEvents:UIControlEventTouchUpOutside];
     [self.recordButton addTarget:self action:@selector(recordButtonTouchUpInside) forControlEvents:UIControlEventTouchUpInside];
     [self.recordButton addTarget:self action:@selector(recordDragOutside) forControlEvents:UIControlEventTouchDragExit];
     [self.recordButton addTarget:self action:@selector(recordDragInside) forControlEvents:UIControlEventTouchDragEnter];
-    self.recordButton.hidden = YES;
-    self.recordButton.center = CGPointMake(CGRectGetWidth(self.toolbarView.frame)/2.0, CGRectGetHeight(self.toolbarView.frame)/2.0);
+    self.recordButton.center = CGPointMake((CGRectGetWidth(self.toolbarView.frame)-34)/2.0, CGRectGetHeight(self.toolbarView.frame)/2.0);
     
     if (!self.recordView) {
         self.recordView = [[DXRecordView alloc] initWithFrame:CGRectMake(90, 130, 140, 140)];
