3979bfd23afe8d7cfc0a794c7b42963f0397fad7
Hacker-liang <pengshuai.liang@gmai.com>
2015-05-20 15:45:12 +0800

    添加 swift bridging 文件

---
+++ PeachTravel/PeachTravel-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "FMDB.h"
+#import "AESCrypt.h"
+#import "GexinSdk.h"
+#import "AFNetworking.h"
+#import "SVProgressHUD.h"
+#import "QiniuSDK.h"
+#import "VoiceConverter.h"
+#import "AppDelegate.h"
