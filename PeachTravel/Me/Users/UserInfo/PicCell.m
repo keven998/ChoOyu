3c5ae41070924d9ddecb50c49f399b6892b3a4af
Hacker-liang <pengshuai.liang@gmai.com>
2015-05-25 10:56:35 +0800

    实现修改 avatar delegate

--- PeachTravel/Me/Users/UserInfo/PicCell.m
+++ PeachTravel/Me/Users/UserInfo/PicCell.m
@@ -11,10 +11,6 @@
 @implementation PicCell
 
 - (void)awakeFromNib {
-    // Initialization code
-//    _picImage.layer.borderColor = APP_BORDER_COLOR.CGColor;
-//    _picImage.layer.borderWidth = 0.5;
-//    _picImage.clipsToBounds = YES;
 }
 
 @end
