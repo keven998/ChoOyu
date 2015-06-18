424bca7da91cc4cf490b434dd889cfbdd09c0eb3
罗勇 <luoyong@lvxingpai.cn>
2015-06-18 21:18:04 +0800

    视觉

--- PeachTravel/ToolBox/Home/ToolsHomeViewController.m
+++ PeachTravel/ToolBox/Home/ToolsHomeViewController.m
@@ -26,29 +26,35 @@
     // Do any additional setup after loading the view.
     CGFloat width = CGRectGetWidth(self.view.bounds);
     CGFloat height = (width-20)*0.67;
-    UIImageView *lxpSearchBg = [[UIImageView alloc] initWithFrame:CGRectMake(10, 10, width - 20, height)];
+    
+    UIButton *cardbg = [[UIButton alloc] initWithFrame:CGRectMake(10, 0, width - 20, height)];
+    cardbg.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    [cardbg setBackgroundImage:[[UIImage imageNamed:@"tools_home_card_bg_normal.png"]
+                               resizableImageWithCapInsets:UIEdgeInsetsMake(20,20,20,20)] forState:UIControlStateNormal];
+    [cardbg setBackgroundImage:[[UIImage imageNamed:@"tools_home_card_bg_highlight.png"]
+                                resizableImageWithCapInsets:UIEdgeInsetsMake(20,20,20,20)] forState:UIControlStateHighlighted];
+    [cardbg addTarget:self action:@selector(lxpSearch:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:cardbg];
+
+    UIImageView *lxpSearchBg = [[UIImageView alloc] initWithFrame:CGRectMake(10, 0, cardbg.frame.size.width - 20, height - 2)];
     lxpSearchBg.clipsToBounds = YES;
     lxpSearchBg.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-    lxpSearchBg.backgroundColor = [UIColor whiteColor];
-    lxpSearchBg.userInteractionEnabled = YES;
     lxpSearchBg.image = [UIImage imageNamed:@"lxp_search_bg_normal.png"];
-    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(lxpSearch:)];
-    [lxpSearchBg addGestureRecognizer:tapGesture];
-    [self.view addSubview:lxpSearchBg];
+    [cardbg addSubview:lxpSearchBg];
     
-    UIImageView *searchIC = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, height, height - 20)];
+    UIImageView *searchIC = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, height, height)];
     searchIC.image = [UIImage imageNamed:@"ic_lxp_search_homo.png"];
-    searchIC.center = CGPointMake((width-20)/2.0, height/2.0 - 5);
+    searchIC.center = CGPointMake((width-20)/2.0, height/2.0 - 2.0);
     searchIC.contentMode = UIViewContentModeScaleAspectFit;
-    [lxpSearchBg addSubview:searchIC];
+    [cardbg addSubview:searchIC];
     
-    UILabel *searchText = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 90, 20)];
+    UILabel *searchText = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 108, 20)];
     searchText.textColor = COLOR_TEXT_II;
-    searchText.font = [UIFont systemFontOfSize:14];
+    searchText.font = [UIFont systemFontOfSize:16];
     searchText.text = @"~旅行·搜搜~";
     searchText.textAlignment = NSTextAlignmentCenter;
     searchText.center = CGPointMake((width-20)/2.0, height/2.0 - 20);
-    [lxpSearchBg addSubview:searchText];
+    [cardbg addSubview:searchText];
     
     CGFloat mxh = CGRectGetMaxY(lxpSearchBg.frame) + 10;
     CGFloat w = (width - 30) / 2;
@@ -60,8 +66,8 @@
     [lxpHelper setBackgroundImage:[UIImage imageNamed:@"tools_home_card_bg_highlight.png"] forState:UIControlStateHighlighted];
     [lxpHelper setTitle:@"旅行达人" forState:UIControlStateNormal];
     [lxpHelper setTitleEdgeInsets:UIEdgeInsetsMake(flag.frame.size.height / 2.0, 0, -64, 0)];
-    [lxpHelper setTitleColor:COLOR_TEXT_II forState:UIControlStateNormal];
-    lxpHelper.titleLabel.font = [UIFont systemFontOfSize:13];
+    [lxpHelper setTitleColor:COLOR_TEXT_III forState:UIControlStateNormal];
+    lxpHelper.titleLabel.font = [UIFont systemFontOfSize:16];
     [lxpHelper addTarget:self action:@selector(goLxpHelper) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:lxpHelper];
     
@@ -73,8 +79,8 @@
     [planHelper setBackgroundImage:[UIImage imageNamed:@"tools_home_card_bg_highlight.png"] forState:UIControlStateHighlighted];
     [planHelper setTitle:@"我的计划" forState:UIControlStateNormal];
     [planHelper setTitleEdgeInsets:UIEdgeInsetsMake(flag.frame.size.height / 2.0, 0, -64, 0)];
-    [planHelper setTitleColor:COLOR_TEXT_II forState:UIControlStateNormal];
-    planHelper.titleLabel.font = [UIFont systemFontOfSize:13];
+    [planHelper setTitleColor:COLOR_TEXT_III forState:UIControlStateNormal];
+    planHelper.titleLabel.font = [UIFont systemFontOfSize:16];
     [planHelper addTarget:self action:@selector(goMyPlan) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:planHelper];
 }
