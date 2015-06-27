af129fdef6fdcf6bba5845015ce53f2737949414
dapiao <dpwudi@vip.qq.com>
2015-06-27 16:20:04 +0800

    no message

---
+++ PeachTravel/Common/Category/UIImage+resized.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+resized.h
+//  PeachTravel
+//
+//  Created by dapiao on 15/6/27.
+//  Copyright (c) 2015年 com.aizou.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (resized)
++ (UIImage *)resizedImageWithName:(NSString *)name;
+@end
