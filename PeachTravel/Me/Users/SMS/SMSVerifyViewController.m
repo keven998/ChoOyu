05394a5de3eec202a3f3b14fd2ab3c87b0c7b824
dapiao <dpwudi@vip.qq.com>
2015-07-03 15:46:39 +0800

    验证码界面

--- PeachTravel/Me/Users/SMS/SMSVerifyViewController.m
+++ PeachTravel/Me/Users/SMS/SMSVerifyViewController.m
@@ -14,9 +14,9 @@
     NSInteger count;
 }
 
-@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
-@property (weak, nonatomic) IBOutlet UITextField *verifyCodeTextField;
-@property (weak, nonatomic) IBOutlet UIButton *verifyCodeBtn;
+@property (strong, nonatomic)  UILabel *titleLabel;
+@property (strong, nonatomic)  UITextField *verifyCodeTextField;
+@property (strong, nonatomic)  UIButton *verifyCodeBtn;
 //@property (weak, nonatomic) IBOutlet UIButton *confirmBtn;
 
 @end
@@ -28,23 +28,47 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
  
-    UIBarButtonItem *registerBtn = [[UIBarButtonItem alloc]initWithTitle:@"确认 " style:UIBarButtonItemStylePlain target:self action:@selector(confirm:)];
-    registerBtn.tintColor = APP_THEME_COLOR;
-    self.navigationItem.rightBarButtonItem = registerBtn;
+    UIButton *backBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    backBtn.frame = CGRectMake(13, 34, 15, 15);
+    [backBtn setImage:[UIImage imageNamed:@"login_back_defaut"] forState:UIControlStateNormal];
+    [backBtn setImage:[UIImage imageNamed:@"common_icon_navigaiton_back_highlight"] forState:UIControlStateHighlighted];
+    [self.view addSubview:backBtn];
+    [backBtn addTarget:self action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside];
     
+    
+    
+    self.view.backgroundColor = [UIColor whiteColor];
     count = _coolDown;
     self.navigationItem.title = @"验证";
-    _titleLabel.text = [NSString stringWithFormat:@"已发送验证码短信至%@\n网络有延迟，请耐心等待", _phoneNumber];
+    
+    UIImageView *iconImage = [[UIImageView alloc]initWithFrame:CGRectMake(489/3 * SCREEN_WIDTH/414, 216/3 *SCREEN_HEIGHT/736, 87*SCREEN_HEIGHT/736, 87*SCREEN_HEIGHT/736)];
+    iconImage.image = [UIImage imageNamed:@"icon_little"];
+    [self.view addSubview:iconImage];
+
+    UIView *textFieldBg = [[UIView alloc]initWithFrame:CGRectMake(13, 890/3 *SCREEN_HEIGHT/736, SCREEN_WIDTH-26, 60 * SCREEN_HEIGHT/736)];
+    textFieldBg.layer.borderColor = APP_THEME_COLOR.CGColor;
+    textFieldBg.layer.borderWidth = 1;
+    textFieldBg.layer.cornerRadius = 5;
+    textFieldBg.clipsToBounds = YES;
+    [self.view addSubview:textFieldBg];
+    UIView *devide2 = [[UIView alloc]initWithFrame:CGRectMake(772/3 *SCREEN_WIDTH/414, 0 *SCREEN_HEIGHT / 736, 1, 60 * SCREEN_HEIGHT/736)];
+    devide2.backgroundColor = APP_THEME_COLOR;
+    [textFieldBg addSubview:devide2];
+    
+    
+    _verifyCodeBtn = [[UIButton alloc]initWithFrame:CGRectMake(775/3 *SCREEN_WIDTH/414, 0, 395/3 * SCREEN_WIDTH/414, 59* SCREEN_HEIGHT/736)];
     [_verifyCodeBtn setTitle:[NSString stringWithFormat:@"%ldS",(long)count] forState:UIControlStateNormal];
-    [_verifyCodeBtn setBackgroundImage:[ConvertMethods createImageWithColor:APP_SUB_THEME_COLOR] forState:UIControlStateNormal];
+    [_verifyCodeBtn setBackgroundImage:[ConvertMethods createImageWithColor:UIColorFromRGB(0xfafaf9)]forState:UIControlStateNormal];
     _verifyCodeBtn.clipsToBounds = YES;
-    [_verifyCodeBtn setBackgroundImage:[ConvertMethods createImageWithColor:[UIColor lightGrayColor]] forState:UIControlStateDisabled];
+    [_verifyCodeBtn setBackgroundImage:[ConvertMethods createImageWithColor:[UIColor whiteColor]] forState:UIControlStateDisabled];
+    _verifyCodeBtn.titleLabel.font = [UIFont systemFontOfSize:14];
     [_verifyCodeBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     [_verifyCodeBtn setTitleColor:TEXT_COLOR_TITLE_SUBTITLE forState:UIControlStateDisabled];
     _verifyCodeBtn.enabled = NO;
     _verifyCodeBtn.layer.cornerRadius = 2.0;
     [self startTimer];
     
+    _verifyCodeTextField = [[UITextField alloc]initWithFrame:CGRectMake(10, 0, SCREEN_WIDTH - 50, 60 * SCREEN_HEIGHT / 736)];
     UILabel *ul = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 64.0, _verifyCodeTextField.bounds.size.height - 16.0)];
     ul.text = @" 验证码:";
     ul.textColor = TEXT_COLOR_TITLE;
@@ -53,11 +77,28 @@
     _verifyCodeTextField.leftView = ul;
     _verifyCodeTextField.leftViewMode = UITextFieldViewModeAlways;
     [_verifyCodeTextField becomeFirstResponder];
+    [textFieldBg addSubview:_verifyCodeBtn];
+    [textFieldBg addSubview:_verifyCodeTextField];
+    
+    UIButton *nextBtn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    nextBtn.frame = CGRectMake(13, 1107/3 * SCREEN_HEIGHT/736, SCREEN_WIDTH - 26, 62 * SCREEN_HEIGHT/736);
+    [nextBtn setTitle:@"下一步" forState:UIControlStateNormal];
+    nextBtn.layer.cornerRadius = 4.0;
+    nextBtn.clipsToBounds = YES;
+    [nextBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [nextBtn setBackgroundImage:[ConvertMethods createImageWithColor:APP_THEME_COLOR] forState:UIControlStateNormal];
+    [self.view addSubview:nextBtn];
+    [nextBtn addTarget:self action:@selector(confirm:) forControlEvents:UIControlEventTouchUpInside];
+    
 }
-
+- (void)viewWillAppear:(BOOL)animated
+{
+    self.navigationController.navigationBarHidden = YES;
+}
 - (void)viewWillDisappear:(BOOL)animated
 {
     [super viewWillDisappear:animated];
+    self.navigationController.navigationBarHidden = NO;
     [self stopTimer];
 }
 
@@ -199,6 +240,12 @@
     [self.view endEditing:YES];
     [super touchesEnded:touches withEvent:event];
 }
-
+- (void)goBack {
+    if (self.navigationController.childViewControllers.count > 1) {
+        [self.navigationController popViewControllerAnimated:YES];
+    } else {
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }
+}
 @end
 
