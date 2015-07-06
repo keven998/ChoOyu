a358b990a734fe12084f3d2f6c35d4efe4c828a9
dapiao <dpwudi@vip.qq.com>
2015-07-06 13:59:07 +0800

    添加城市页面  cell修改

---
+++ PeachTravel/TravelPlan/MakeTravel/Destination/Views/DomesticCell.h
@@ -0,0 +1,17 @@
+//
+//  DomesticDestinationCell.h
+//  PeachTravel
+//
+//  Created by liangpengshuai on 14/10/9.
+//  Copyright (c) 2014年 com.aizou.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DomesticCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *tiltleLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *status;
+@property (weak, nonatomic) IBOutlet UIImageView *backGroundImage;
+
+@end
