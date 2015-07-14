4cdd8b9fee206cf7d7822fdcc154f854187c2d42
罗勇 <luoyong@lvxingpai.cn>
2015-07-14 19:01:17 +0800

    一个返回按钮

--- PeachTravel/TravelPlan/MakeTravel/PlansListTableViewController/PlansListTableViewController.m
+++ PeachTravel/TravelPlan/MakeTravel/PlansListTableViewController/PlansListTableViewController.m
@@ -97,7 +97,7 @@ static NSString *reusableCell = @"myGuidesCell";
     }
     
     if (!_selectToSend) {
-        self.navigationItem.leftBarButtonItem = [UIBarButtonItem itemWithIcon:@"common_icon_navigaiton_back_theme_style" highIcon:@"common_icon_navigaiton_back_highlight" target:self action:@selector(goBack)];
+        self.navigationItem.leftBarButtonItem = [UIBarButtonItem itemWithIcon:@"common_icon_navigaiton_back_dark.png" highIcon:@"common_icon_navigaiton_back_highlight" target:self action:@selector(goBack)];
     } else {
         self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"取消" style:UIBarButtonItemStylePlain target:self action:@selector(goBack)];
     }
