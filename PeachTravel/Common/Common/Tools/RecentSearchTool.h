c1973d94d44794cee92252d487693f60c475e7fe
王聪 <2441413514@qq.com>
2015-08-10 16:17:01 +0800

    新建最近搜索数据库,添加获取最近搜索结果工具类

--- PeachTravel/Common/Common/Tools/RecentSearchTool.h
+++ PeachTravel/Common/Common/Tools/RecentSearchTool.h
@@ -10,4 +10,10 @@
 
 @interface RecentSearchTool : NSObject
 
+// 将搜索结果插入到数据库中
++ (void)saveRecentSearchToSpoiData:(NSString *)recentSearch;
+
+// 读取数据库中的元素
++ (NSArray *)getAllRecentSearchResult;
+
 @end
--- PeachTravel/Common/Common/Tools/RecentSearchTool.m
+++ PeachTravel/Common/Common/Tools/RecentSearchTool.m
@@ -7,7 +7,47 @@
 //
 
 #import "RecentSearchTool.h"
+#import "FMDatabase.h"
 
 @implementation RecentSearchTool
+
+// 数据库实例
+static FMDatabase * _db;
+
++ (void)initialize
+{
+    // 1.获得数据库文件的路径
+    NSString * doc = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+    NSString * filename = [doc stringByAppendingPathComponent:@"SpiData.sqlite"];
+    
+    // 2.得到数据库
+    _db = [FMDatabase databaseWithPath:filename];
+    
+    // 3.打开数据库
+    if ([_db open]) {
+        // 4.创表
+        BOOL result = [_db executeUpdate:@"create table if not exists recent_search(id integer primary key autoincrement,search_content text not null);"];
+        if (result) {
+            NSLog(@"创表成功");
+        }else{
+            NSLog(@"创表失败");
+        }
+    }
+}
+
+
+// 将搜索结果插入到数据库中
++ (void)saveRecentSearchToSpoiData:(NSString *)recentSearch
+{
+
+}
+
+// 读取数据库中的元素
++ (NSArray *)getAllRecentSearchResult{
+    
+    NSMutableArray * recentSearchResult = [NSMutableArray array];
+    
+    return recentSearchResult;
+}
 
 @end
