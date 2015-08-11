b992a59634268fc1715e8044d7c601b25cabfa6a
王聪 <2441413514@qq.com>
2015-08-11 21:10:57 +0800

    修改图片

--- PeachTravel/TravelPlan/MakeTravel/PlansListTableViewController/PlansListTableViewCell/PlansListTableHeaderView.m
+++ PeachTravel/TravelPlan/MakeTravel/PlansListTableViewController/PlansListTableViewCell/PlansListTableHeaderView.m
@@ -17,7 +17,8 @@
 
 - (void)awakeFromNib
 {
-    self.bgImageView.image = [[UIImage imageNamed:@"plan_bg_add"] resizableImageWithCapInsets:UIEdgeInsetsMake(41, 20, 5, 181) resizingMode:UIImageResizingModeStretch];
+    self.bgImageView.image = [[UIImage imageNamed:@"plan_bg_add"] resizableImageWithCapInsets:UIEdgeInsetsMake(41, 12, 100, 187) resizingMode:UIImageResizingModeStretch];
+//    self.bgImageView.image = [UIImage imageNamed:@"plan_bg_add"];
     
     [self.addTourPlan setTitleColor:APP_THEME_COLOR forState:UIControlStateNormal];
 }
