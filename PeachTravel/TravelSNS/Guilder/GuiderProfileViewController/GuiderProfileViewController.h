ce3cb7fb1e5dfed361cee935eb48e846072ceea7
王聪 <2441413514@qq.com>
2015-08-28 00:58:27 +0800

    达人Profile

---
+++ PeachTravel/TravelSNS/Guilder/GuiderProfileViewController/GuiderProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  GuiderProfileViewController.h
+//  PeachTravel
+//
+//  Created by 王聪 on 8/27/15.
+//  Copyright (c) 2015 com.aizou.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GuiderProfileViewController : UIViewController
+
+@end
---
+++ PeachTravel/TravelSNS/Guilder/GuiderProfileViewController/GuiderProfileViewController.m
@@ -0,0 +1,25 @@
+//
+//  GuiderProfileViewController.m
+//  PeachTravel
+//
+//  Created by 王聪 on 8/27/15.
+//  Copyright (c) 2015 com.aizou.www. All rights reserved.
+//
+
+#import "GuiderProfileViewController.h"
+
+@interface GuiderProfileViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation GuiderProfileViewController
+
+#pragma mark - lifeCycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+@end
