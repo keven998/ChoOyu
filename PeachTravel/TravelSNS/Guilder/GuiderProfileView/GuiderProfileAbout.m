b750ec12353e4652378cd8e7598bce6d8ef858c6
王聪 <2441413514@qq.com>
2015-08-28 19:49:52 +0800

    旅行,相册

---
+++ PeachTravel/TravelSNS/Guilder/GuiderProfileView/GuiderProfileAbout.m
@@ -0,0 +1,23 @@
+//
+//  GuiderProfileAbout.m
+//  PeachTravel
+//
+//  Created by 王聪 on 8/28/15.
+//  Copyright (c) 2015 com.aizou.www. All rights reserved.
+//
+
+#import "GuiderProfileAbout.h"
+
+@implementation GuiderProfileAbout
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
