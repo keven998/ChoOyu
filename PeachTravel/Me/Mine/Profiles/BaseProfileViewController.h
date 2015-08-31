789f38758f80f1acafc2a8a6a078308c59f9012b
王聪 <2441413514@qq.com>
2015-08-31 14:25:14 +0800

    抽取Profile基类代码

---
+++ PeachTravel/Me/Mine/Profiles/BaseProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseProfileViewController.h
+//  PeachTravel
+//
+//  Created by 王聪 on 8/31/15.
+//  Copyright (c) 2015 com.aizou.www. All rights reserved.
+//
+
+#import "TZViewController.h"
+
+@interface BaseProfileViewController : TZViewController
+
+@end
---
+++ PeachTravel/Me/Mine/Profiles/BaseProfileViewController.m
@@ -0,0 +1,37 @@
+//
+//  BaseProfileViewController.m
+//  PeachTravel
+//
+//  Created by 王聪 on 8/31/15.
+//  Copyright (c) 2015 com.aizou.www. All rights reserved.
+//
+
+#import "BaseProfileViewController.h"
+
+@interface BaseProfileViewController ()
+
+@end
+
+@implementation BaseProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
