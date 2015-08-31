a3723a9b91b42accce090dbda5659cc1c748f442
王聪 <2441413514@qq.com>
2015-08-31 20:38:34 +0800

    删除多余图片

---
+++ PeachTravel/libs/CWDropdownMenu.h
@@ -0,0 +1,13 @@
+//
+//  CWDropdownMenu.h
+//  PeachTravel
+//
+//  Created by 王聪 on 15/8/31.
+//  Copyright (c) 2015年 com.aizou.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CWDropdownMenu : UIView
+
+@end
---
+++ PeachTravel/libs/CWDropdownMenu.m
@@ -0,0 +1,21 @@
+//
+//  CWDropdownMenu.m
+//  PeachTravel
+//
+//  Created by 王聪 on 15/8/31.
+//  Copyright (c) 2015年 com.aizou.www. All rights reserved.
+//
+
+#import "CWDropdownMenu.h"
+
+@implementation CWDropdownMenu
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
