1585bd7e88e13d559f4e8aadfcf6f5b22bb75b4a
王聪 <2441413514@qq.com>
2015-09-01 09:46:15 +0800

    下拉菜单

--- PeachTravel/libs/CWDropdownMenu.h
+++ PeachTravel/libs/CWDropdownMenu.h
@@ -7,7 +7,35 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "UIView+Extension.h"
+@class CWDropdownMenu;
+
+@protocol CWDropdownMenuDelegate <NSObject>
+@optional
+- (void)dropdownMenuDidDismiss:(CWDropdownMenu *)menu;
+- (void)dropdownMenuDidShow:(CWDropdownMenu *)menu;
+@end
 
 @interface CWDropdownMenu : UIView
+@property (nonatomic, weak) id<CWDropdownMenuDelegate> delegate;
 
++ (instancetype)menu;
+
+/**
+ *  显示
+ */
+- (void)showFrom:(UIView *)from;
+/**
+ *  销毁
+ */
+- (void)dismiss;
+
+/**
+ *  内容
+ */
+@property (nonatomic, strong) UIView *content;
+/**
+ *  内容控制器
+ */
+@property (nonatomic, strong) UIViewController *contentController;
 @end
