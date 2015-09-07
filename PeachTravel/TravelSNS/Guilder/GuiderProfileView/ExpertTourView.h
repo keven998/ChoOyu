20ab939e0fa392681a40380fd7e7998e85df763f
王聪 <2441413514@qq.com>
2015-09-07 15:06:48 +0800

    抽取代码

---
+++ PeachTravel/TravelSNS/Guilder/GuiderProfileView/ExpertTourView.h
@@ -0,0 +1,13 @@
+//
+//  ExpertTourView.h
+//  PeachTravel
+//
+//  Created by 王聪 on 9/7/15.
+//  Copyright (c) 2015 com.aizou.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExpertTourView : UIView
+
+@end
---
+++ PeachTravel/TravelSNS/Guilder/GuiderProfileView/ExpertTourView.m
@@ -0,0 +1,21 @@
+//
+//  ExpertTourView.m
+//  PeachTravel
+//
+//  Created by 王聪 on 9/7/15.
+//  Copyright (c) 2015 com.aizou.www. All rights reserved.
+//
+
+#import "ExpertTourView.h"
+
+@implementation ExpertTourView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
