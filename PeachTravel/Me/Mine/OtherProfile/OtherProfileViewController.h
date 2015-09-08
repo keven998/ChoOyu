801f1f85c4abb15cbab6d975350d12fff3fde41a
王聪 <2441413514@qq.com>
2015-09-08 10:47:01 +0800

    他人Profile

---
+++ PeachTravel/Me/Mine/OtherProfile/OtherProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  OtherProfileViewController.h
+//  PeachTravel
+//
+//  Created by 王聪 on 9/8/15.
+//  Copyright (c) 2015 com.aizou.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OtherProfileViewController : UIViewController
+
+@end
