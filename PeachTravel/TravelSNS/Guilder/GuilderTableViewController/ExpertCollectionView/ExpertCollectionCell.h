3728afebc9611c632673cc54e97ad81ad120c579
王聪 <2441413514@qq.com>
2015-09-10 21:01:22 +0800

    添加背景图片

---
+++ PeachTravel/TravelSNS/Guilder/GuilderTableViewController/ExpertCollectionView/ExpertCollectionCell.h
@@ -0,0 +1,13 @@
+//
+//  ExpertCollectionCell.h
+//  PeachTravel
+//
+//  Created by 王聪 on 9/10/15.
+//  Copyright (c) 2015 com.aizou.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExpertCollectionCell : UITableViewCell
+
+@end
---
+++ PeachTravel/TravelSNS/Guilder/GuilderTableViewController/ExpertCollectionView/ExpertCollectionCell.m
@@ -0,0 +1,23 @@
+//
+//  ExpertCollectionCell.m
+//  PeachTravel
+//
+//  Created by 王聪 on 9/10/15.
+//  Copyright (c) 2015 com.aizou.www. All rights reserved.
+//
+
+#import "ExpertCollectionCell.h"
+
+@implementation ExpertCollectionCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
