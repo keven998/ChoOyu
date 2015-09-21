6262e44df2f9cb3f87d2676cb95f9578fc7cc68d
Hacker-liang <pengshuai.liang@gmai.com>
2015-09-21 11:27:47 +0800

    修改 useralbum 的解析

--- PeachTravel/Me/Users/UserInfo/AlbumImageModel.m
+++ PeachTravel/Me/Users/UserInfo/AlbumImageModel.m
@@ -14,11 +14,17 @@
 {
     if (self = [super init]) {
         _imageId = [json objectForKey:@"id"];
-        if ([[json objectForKey:@"image"] objectForKey:@"caption"] != [NSNull null]) {
-            _imageDesc = [[json objectForKey:@"image"] objectForKey:@"caption"];
+        if ([[[json objectForKey:@"image"] firstObject] objectForKey:@"caption"] != [NSNull null]) {
+            _imageDesc = [[[json objectForKey:@"image"] firstObject] objectForKey:@"caption"];
         }
-        _imageUrl = [[json objectForKey:@"image"] objectForKey:@"url"];
+        _imageUrl = [[[json objectForKey:@"image"] firstObject] objectForKey:@"url"];
+        if ([[[json objectForKey:@"image"] firstObject] objectForKey:@"smallUrl"]) {
+            _smallImageUrl = [[[json objectForKey:@"image"] firstObject] objectForKey:@"smallUrl"];
+        } else {
+            _smallImageUrl = _imageUrl;
+        }
         _createTime = [[json objectForKey:@"cTime"] longValue];
+        
     }
     return self;
 }
