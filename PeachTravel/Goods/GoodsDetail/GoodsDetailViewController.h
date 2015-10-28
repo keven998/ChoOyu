4ddd8ba17cdf139b55782c616696b9097f5299a9
Hacker-liang <pengshuai.liang@gmai.com>
2015-10-28 19:01:22 +0800

    订单详情

---
+++ PeachTravel/Goods/GoodsDetail/GoodsDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  GoodsDetailViewController.h
+//  PeachTravel
+//
+//  Created by liangpengshuai on 10/26/15.
+//  Copyright © 2015 com.aizou.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GoodsDetailViewController : UIViewController
+
+@end
---
+++ PeachTravel/Goods/GoodsDetail/GoodsDetailViewController.m
@@ -0,0 +1,71 @@
+//
+//  GoodsDetailViewController.m
+//  PeachTravel
+//
+//  Created by liangpengshuai on 10/26/15.
+//  Copyright © 2015 com.aizou.www. All rights reserved.
+//
+
+#import "GoodsDetailViewController.h"
+#import "RCTRootView.h"
+#import "RCTBridgeModule.h"
+
+@interface GoodsDetailViewController ()<RCTBridgeModule>
+
+
+@end
+
+@implementation GoodsDetailViewController
+
+RCT_EXPORT_MODULE();
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    self.view.backgroundColor = [UIColor whiteColor];
+    NSURL *jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/src/goodsDetailContainer.bundle?platform=ios&dev=true"];
+
+    RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                        moduleName:@"GoodsDetailContainer"
+                                                 initialProperties:nil
+                                                     launchOptions:nil];
+    rootView.frame = CGRectMake(0, 0, kWindowWidth, kWindowHeight);
+    [self.view addSubview:rootView];
+
+}
+
+RCT_EXPORT_METHOD(makePhone:(NSString *)tel){
+    NSString *number = tel;// 此处读入电话号码
+    NSString *num = [[NSString alloc] initWithFormat:@"tel://%@",number];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:num]];//打电话
+
+}
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
+}
+
+- (void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
