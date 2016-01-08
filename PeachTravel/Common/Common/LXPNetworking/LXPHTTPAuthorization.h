f7e50e60bf48d8b9f591f71e48f17cbc4f54eaad
Hacker-liang <pengshuai.liang@gmai.com>
2016-01-08 10:01:34 +0800

    添加 http 鉴权文件

---
+++ PeachTravel/Common/Common/LXPNetworking/LXPHTTPAuthorization.h
@@ -0,0 +1,13 @@
+//
+//  LXPHTTPAuthorization.h
+//  PeachTravel
+//
+//  Created by liangpengshuai on 1/7/16.
+//  Copyright © 2016 com.aizou.www. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LXPHTTPAuthorization : NSObject
+
+@end
---
+++ PeachTravel/Common/Common/LXPNetworking/LXPHTTPAuthorization.m
@@ -0,0 +1,13 @@
+//
+//  LXPHTTPAuthorization.m
+//  PeachTravel
+//
+//  Created by liangpengshuai on 1/7/16.
+//  Copyright © 2016 com.aizou.www. All rights reserved.
+//
+
+#import "LXPHTTPAuthorization.h"
+
+@implementation LXPHTTPAuthorization
+
+@end
