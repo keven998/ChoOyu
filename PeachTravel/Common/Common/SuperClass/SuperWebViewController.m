7ea717cb3ac67cf23bbdd2696376e3ccb54698a8
Hacker-liang <pengshuai.liang@gmai.com>
2016-03-23 14:43:14 +0800

    bug fix

--- PeachTravel/Common/Common/SuperClass/SuperWebViewController.m
+++ PeachTravel/Common/Common/SuperClass/SuperWebViewController.m
@@ -11,12 +11,19 @@
 #import "NJKWebViewProgressView.h"
 #import "TZSchemeManager.h"
 #import "GoodsDetailViewController.h"
+#import "UMSocial.h"
+#import "LoginViewController.h"
+#import "ChatRecoredListTableViewController.h"
+#import "TaoziChatMessageBaseViewController.h"
+#import "ShareActivity.h"
 
-@interface SuperWebViewController () <UIWebViewDelegate, NJKWebViewProgressDelegate> {
+@interface SuperWebViewController () <UIWebViewDelegate, NJKWebViewProgressDelegate, ActivityDelegate, CreateConversationDelegate, TaoziMessageSendDelegate> {
     
     NJKWebViewProgressView *_progressView;
     NJKWebViewProgress *_progressProxy;
 }
+
+@property (nonatomic, strong) ChatRecoredListTableViewController *chatRecordListCtl;
 
 @end
 
@@ -62,6 +69,10 @@
     label.numberOfLines = 2;
     [self.webView addSubview:label];
     [self.webView bringSubviewToFront:self.webView.scrollView];
+    
+    if ([_urlStr containsString:@"mycode.php"]) {
+        [self setupNaviBar];
+    }
 }
 
 - (void)goBack
@@ -95,6 +106,14 @@
     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
     self.webView.delegate = nil;
     self.webView = nil;
+}
+
+- (void)setupNaviBar
+{
+    UIButton *shareBtn = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 30, 40)];
+    [shareBtn setImage:[UIImage imageNamed:@"icon_share_white"] forState:UIControlStateNormal];
+    [shareBtn addTarget:self action:@selector(share2Frend) forControlEvents:UIControlEventTouchUpInside];
+    self.navigationItem.rightBarButtonItems = @[[[UIBarButtonItem alloc] initWithCustomView:shareBtn]];
 }
 
 - (void)didStartLoadURLRequest:(NSURLRequest *)request
@@ -133,5 +152,89 @@
 {
     [_progressView setProgress:progress animated:YES];
 }
+
+- (void)share2Frend
+{
+    NSArray *shareButtonimageArray = @[@"ic_sns_pengyouquan.png",  @"ic_sns_weixin.png", @"ic_sns_qq.png", @"ic_sns_sina.png", @"ic_sns_douban.png"];
+    //    NSArray *shareButtonimageArray = @[@"ic_sns_lxp.png", @"ic_sns_pengyouquan.png",  @"ic_sns_weixin.png", @"ic_sns_qq.png"];
+    
+    NSArray *shareButtonTitleArray = @[@"朋友圈", @"微信朋友", @"QQ", @"新浪微博", @"豆瓣"];
+    ShareActivity *shareActivity = [[ShareActivity alloc] initWithTitle:@"转发至" delegate:self cancelButtonTitle:@"取消" ShareButtonTitles:shareButtonTitleArray withShareButtonImagesName:shareButtonimageArray];
+    [shareActivity showInView:self.navigationController.view];
+}
+
+#pragma mark - AvtivityDelegate
+
+- (void)didClickOnImageIndex:(NSInteger)imageIndex
+{
+    NSString *url = [NSString stringWithFormat:@"%@&share=1", _urlStr];
+    NSString *shareTitle = @"旅行派特色体验";
+    NSString *shareContentWithoutUrl = @"邀请好友注册，领支付宝现金红包，1000元等你来抢！";
+    NSString *shareContentWithUrl = [NSString stringWithFormat:@"邀请好友注册，领支付宝现金红包，1000元等你来抢！ %@", _urlStr];
+    
+    UMSocialUrlResource *resource = [[UMSocialUrlResource alloc] initWithSnsResourceType:UMSocialUrlResourceTypeImage url:nil];
+    
+    [UMSocialConfig setFinishToastIsHidden:NO position:UMSocialiToastPositionCenter];
+    switch (imageIndex) {
+            
+        case 0: {
+            [UMSocialData defaultData].extConfig.wechatTimelineData.url = url;
+            [UMSocialData defaultData].extConfig.wechatTimelineData.title = shareTitle;
+            
+            [[UMSocialDataService defaultDataService]  postSNSWithTypes:@[UMShareToWechatTimeline] content:shareContentWithoutUrl image:[UIImage imageNamed:@"share_icon.png"] location:nil urlResource:resource presentedController:self completion:^(UMSocialResponseEntity *response){
+                if (response.responseCode == UMSResponseCodeSuccess) {
+                    NSLog(@"分享成功！");
+                }
+            }];
+        }
+            break;
+            
+        case 1: {
+            [UMSocialData defaultData].extConfig.wechatSessionData.url = url;
+            [UMSocialData defaultData].extConfig.wechatSessionData.title = shareTitle;
+            [[UMSocialDataService defaultDataService]  postSNSWithTypes:@[UMShareToWechatSession] content:shareContentWithoutUrl image:[UIImage imageNamed:@"share_icon.png"] location:nil urlResource:resource presentedController:nil completion:^(UMSocialResponseEntity *response){
+                if (response.responseCode == UMSResponseCodeSuccess) {
+                    NSLog(@"分享成功！");
+                }
+            }];
+        }
+            break;
+            
+        case 2: {
+            [UMSocialData defaultData].extConfig.qqData.url = url;
+            [UMSocialData defaultData].extConfig.qqData.title = shareTitle;
+            
+            [[UMSocialDataService defaultDataService]  postSNSWithTypes:@[UMShareToQQ] content:shareContentWithoutUrl image:[UIImage imageNamed:@"share_icon.png"] location:nil urlResource:resource presentedController:self completion:^(UMSocialResponseEntity *response){
+                if (response.responseCode == UMSResponseCodeSuccess) {
+                    NSLog(@"分享成功！");
+                }
+            }];
+        }
+            break;
+            
+        case 3:
+            
+            [UMSocialData defaultData].extConfig.sinaData.urlResource = resource;
+            
+            [[UMSocialDataService defaultDataService]  postSNSWithTypes:@[UMShareToSina] content:shareContentWithUrl image:[UIImage imageNamed:@"share_icon.png"] location:nil urlResource:resource presentedController:self completion:^(UMSocialResponseEntity *response){
+                if (response.responseCode == UMSResponseCodeSuccess) {
+                    NSLog(@"分享成功！");
+                }
+            }];
+            
+            break;
+            
+        case 4:
+            [[UMSocialControllerService defaultControllerService] setShareText:shareContentWithUrl shareImage:[UIImage imageNamed:@"share_icon.png"]  socialUIDelegate:nil];
+            
+            [UMSocialSnsPlatformManager getSocialPlatformWithName:UMShareToDouban].snsClickHandler(self,[UMSocialControllerService defaultControllerService],YES);
+            
+            break;
+            
+        default:
+            break;
+    }
+}
+
 
 @end
